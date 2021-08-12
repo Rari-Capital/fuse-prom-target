cb1cf3ed355ef1e175813f7f405dcfde145d3b01
t11s <greenfeatherhelp@gmail.com>
2021-08-11 22:12:36 -0700

    Restart every hour

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -4,11 +4,12 @@ module.exports = {
       name: "fuse-prom-target",
       script: "npm",
       args: "run start-target",
+      cron_restart: "0 * * * *" // Restart every hour.
     },
     {
       name: "fuse-prom-proxy",
       script: "npm",
-      args: "run start-proxy",
-    },
-  ],
+      args: "run start-proxy"
+    }
+  ]
 };
